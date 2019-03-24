13a3566f818deeb79b960d26239f354299232c71
victoria <victoria-butrym@tut.by>
2019-03-24 16:58:42 +0300

    feat: solution using while

--- src/index.js
+++ src/index.js
@@ -1,3 +1,13 @@
 module.exports = function getZerosCount(number, base) {
   // your implementation
+  
+  let trailing_zeros = 0;
+  
+
+   while (number % base == 0){
+    trailing_zeros++;
+    number = number / base;      
+  }
+ 
+  return trailing_zeros;
 }
